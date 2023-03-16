954eb83dd9335480367a2af7d407c4b7c3e4d18a
dhanushhhhhhh <112607475+dhanushhhhhhh@users.noreply.github.com>
2023-03-17 00:31:27 +0530

    Create 3Sum.py

---
+++ 3Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans=[]
+        for n,i in enumerate(nums):
+            j,k=n+1,len(nums)-1
+            if(n>0 and i==nums[n-1]): continue
+            while(j<k):
+                    sum=i+nums[j]+nums[k]
+                    if(sum==0):
+                        ans.append([i,nums[j],nums[k]])
+                        j+=1
+                        while nums[j]==nums[j-1] and j<k:j+=1
+                    elif(sum<0):  
+                        j+=1
+                    else:
+                        k-=1
+        return ans
