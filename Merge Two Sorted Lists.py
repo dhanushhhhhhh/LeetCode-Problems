6602ce45405490e586599dd6c6cbec8b94742502
dhanushhhhhhh <112607475+dhanushhhhhhh@users.noreply.github.com>
2023-03-17 00:43:39 +0530

    Create Merge Two Sorted Lists.py

---
+++ Merge Two Sorted Lists.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        head = dummy
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                head.next = list1
+                list1 = list1.next
+            else:
+                head.next = list2
+                list2 = list2.next
+            head = head.next
+        head.next = list1 if list1 is not None else list2
+        return dummy.next
